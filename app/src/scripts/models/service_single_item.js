b78d06b116e497462bc1327e419ad47c785bdfa4
doronzzz <gourdoron@gmail.com>
2015-05-26 14:20:18 -0700

    nocl

--- app/src/scripts/models/service_single_item.js
+++ app/src/scripts/models/service_single_item.js
@@ -18,7 +18,7 @@ define(["backbone","constants"],function(Backbone,constants){
             if(apiKey !== "false"){
                 str += "?api_key=" + apiKey;
             }
-              if(this.params['api-params']){
+            if(this.params['api-params']){
                 var customApiParams = JSON.parse(this.params['api-params']);
                 _.forEach(customApiParams,function(v,k){
                     str += "&"+k+"="+v;
