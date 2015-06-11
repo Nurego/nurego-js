fca327ee79216882e0f76ee0c2ba76fcbe08027d
Meital Federgreen <zask.mz@gmail.com>
2015-06-11 13:18:11 +0300

    Added iframe name

--- app/src/scripts/services/widget_factory.js
+++ app/src/scripts/services/widget_factory.js
@@ -20,6 +20,9 @@ define(["underscore","utils","constants","jquery"],function(_,utils,constants,$N
 			if(typeof(opt.configParams.uid) != "undefined"){
 				iframe.id = opt.configParams.uid;
 			}
+			if(typeof(opt.configParams.fname) != "undefined"){
+				iframe.name = opt.configParams.fname;
+			}
 			this.decorateIframe(iframe);
 			$Nurego(opt.element).append(iframe);
 		},
