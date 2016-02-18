b9e650168392d5e8943f2fa15b1b84bd21b437fa
doron <gourdoron@gmail.com>
2016-02-18 17:45:23 +0200

    features text indentation + missing commas + center table to midde of page

--- app/src/scripts/models/price_list.js
+++ app/src/scripts/models/price_list.js
@@ -28,8 +28,6 @@ define(["backbone","constants"],function(Backbone,constants){
             }
 
             return url;
-
-        	//return "https://api.nurego.com/v1/offerings?api_key=lc14de81-587e-49d8-ba0e-487498ae297a&callback=jQuery19108296897902619094_1424775818134&_=1424775818135";
         },
 
         parse:function(data,req){
