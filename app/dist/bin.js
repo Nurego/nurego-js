9bb2ad1f10839a3563e31344a17aff1650635035
eyalezer <eyalezer@gmail.com>
2016-12-01 15:19:39 +0200

    Fixed api request @ service single item

--- app/dist/bin.js
+++ app/dist/bin.js
@@ -12038,16 +12038,16 @@ singleItemModel = function (Backbone, constants) {
     url: function () {
       //var str = constants.nuregoApiUrl() + "/legaldocs/";
       //var str = constants.nuregoApiUrl() + "/catalog";
-      var str = constants.nuregoApiUrl() + '/services/' + this.params['service_id'];
+      var str = constants.nuregoApiUrl() + '/services/' + this.params['service_id'] + '?';
       //var str = "/src/scripts/mockdata/category.json";
       var apiKey = constants.getNuregoApiKey();
       if (apiKey !== 'false') {
-        str += '?api_key=' + apiKey;
+        str += 'api_key=' + apiKey + '&';
       }
       if (this.params['api-params']) {
         var customApiParams = JSON.parse(this.params['api-params']);
         _.forEach(customApiParams, function (v, k) {
-          str += '&' + k + '=' + v;
+          str += k + '=' + v + '&';
         });
       }
       return str;
